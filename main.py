733a9a175535cd422c4dfb0b035564d2b7d655ca
kniteofdark <dp6vaughn@gmail.com>
2022-10-09 23:45:18 -0500

    more work

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, json
 from herokudb import Heroku
 
 app = Flask(__name__)
@@ -14,7 +14,34 @@ def entryScreen():
 def home():
     return render_template("EntryScreen.html")
 
-@app.route("/enter", methods = ['POST'])
+@app.route("/link", methods = ['POST'])
+def enterPlayers():
+    
+    playerInformation = []
+
+    if request.method == 'POST':
+
+        playerData = request.get_json(force = True)
+        for i in playerData.keys():
+            for j in playerData.values():
+                playerInformation.append(i)
+                playerInformation.append(j)
+        
+        id = playerData["red1ID"]
+        firstName = playerData["red1fname"]
+        lastName = playerData["red1lname"]
+        codename = playerData["red1code"]
+
+        herokudb.playerAdd(id,firstName,lastName,codename)
+
+        id = playerData["green1ID"]
+        firstName = playerData["green1fname"]
+        lastName = playerData["green1lname"]
+        codename = playerData["green1code"]
+
+        herokudb.playerAdd(id,firstName,lastName,codename)
+
+    return jsonify(playerData)
 
 if __name__ == '__main__':
     app.run(debug=True)
